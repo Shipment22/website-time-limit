f7e8baf2fdfec236049b245833e1f2f82957d99d
Shipment22 <crapple2@datazap.net>
2021-12-22 18:36:11 -0500

    content script seems to work

--- content_script.js
+++ content_script.js
@@ -1,4 +1,45 @@
 console.log('Website Time Limit')
 
-// check time
-// make used time go up
+let updateInterval;
+
+if (document.visibilityState === 'visible') {
+    updateInterval = setInterval(update, 60000)
+}
+
+document.addEventListener("visibilitychange", function() {
+    if (document.visibilityState === 'hidden') {
+        clearInterval(updateInterval)
+        updateInterval = null
+    } else {
+        updateInterval = setInterval(update, 60000)
+    }
+})
+
+function redirect() {
+    let url = chrome.extension.getURL('goback/index.html')
+    let origin = window.location.origin
+    let pathname = window.location.pathname
+    window.location.replace(`${url}?origin=${origin}&pathname=${pathname}`)
+}
+
+function checkTime(sock) {
+    let limit = (sock.hours * 6) + sock.minutes
+    if (sock.used > limit) {
+        redirect()
+    }
+}
+
+async function update() {
+    let loc = window.location.origin
+
+    let stuffs = await chrome.storage.local.get(loc)
+    let sock = stuffs[loc]
+
+    if (!stuffs || sock.hours <= 0 || sock.minutes <= 0) return
+
+    sock.used ++
+
+    checkTime(sock)
+
+    chrome.storage.local.set(stuffs)
+}
