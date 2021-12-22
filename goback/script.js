c9bbf712866d2beb1d102883b19ee6f447aff571
Shipment22 <crapple2@datazap.net>
2021-12-22 18:35:01 -0500

    "go back" page seems to work

--- goback/script.js
+++ goback/script.js
@@ -1,20 +1,29 @@
-let params = window.location.search;
+let params = window.location.search
 
-params = params.slice(5, params.length).split('&');
+params = params.slice(8, params.length).split('&')
 
-let domain = params[0];
+let origin = params[0]
 
-let page = params[1].slice(5, params[1].length);
+let path = params[1].slice(9, params[1].length)
 
-document.querySelector('h1 span').textContent = domain;
+let url = origin + path
+
+document.querySelector('h1 span').textContent = origin
 
 document.getElementById('goback').onclick = function() {
-    window.history.back();
+    window.history.back()
 };
 
 document.getElementById('continue').onclick = function() {
-    if (prompt('Type "Yes i want to over ride the time limit." to continue.') === 'Yes i want to over ride the time limit.') {
-        // give more time
-        window.location.replace(domain + page);
+    if (prompt('Type "Yes i want to over ride the time limit." to continue (snooze the time for 15min).') === 'Yes i want to over ride the time limit.') {
+        
+        chrome.storage.local.get(origin, stuffs => {
+            let things = stuffs[origin]
+            things.used -= 15
+            stuffs[origin] = things
+            chrome.storage.local.set(stuffs)
+        })
+
+        window.location.replace(url);
     }
 };
